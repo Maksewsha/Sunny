953caabbdfa48eb54df2ff3298ba90b67838ceae
Maksewsha <makasiz.makson@gmail.com>
2022-08-02 12:39:44 +0300

    Initial Commit

---
+++ Sunny/Supporting files/Constans.swift
@@ -0,0 +1,11 @@
+//
+//  Constans.swift
+//  Sunny
+//
+//  Created by admin on 31.07.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "538a145cf311f3d52c6c6ccc61c5b933"
